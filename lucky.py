a1493c260e05294d4edff8d5d95fd6a51116b47e
GabryelBR <151447391+GabryelBR@users.noreply.github.com>
2023-11-22 10:58:11 -0300

    Update and rename exercicio58.py to lucky.py

--- lucky.py
+++ lucky.py
@@ -1,4 +1,3 @@
-#Exercício Python 58: Melhore o jogo do DESAFIO 28 onde o computador vai “pensar” em um número entre 0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vence
 import random
 import time
 print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\nVou pensar em um número de 0 a 10 tente advinhar !\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
